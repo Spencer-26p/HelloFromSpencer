fd60d177d466e4c1c3b71c3181afe2d64332db37
Elaine Henson <hen20028@byui.edu>
2021-05-20 12:45:14 +0100

    Adding message from Elaine

--- src/prove04/HelloFromSpencer.java
+++ src/prove04/HelloFromSpencer.java
@@ -4,5 +4,6 @@ public class HelloFromSpencer {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Spencer Pace.");
+        System.out.println("Hello Spencer, my name is Elaine.");
     }
 }
